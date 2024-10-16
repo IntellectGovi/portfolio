9290a1385a97751d27f559684ee3a0a6807856fb
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2024-10-16 14:41:46 +0530

    First Commit

---
+++ src/screens/BootScreen.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import BootElement from "@/components/Landing/BootElement";
+import { AnimatePresence } from "framer-motion";
+import React, { useEffect, useState } from "react";
+
+const BootScreen = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = "default";
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
+  }, []);
+  return (
+    <AnimatePresence mode="wait">{isLoading && <BootElement />}</AnimatePresence>
+  );
+};
+
+export default BootScreen;
---
+++ src/types/dockTypes.tsx
@@ -0,0 +1,7 @@
+export type dockType = {
+    title: string;
+    icon: string;
+    href: string;
+    newTab: boolean;
+  };
+  
