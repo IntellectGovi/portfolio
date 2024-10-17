5fc85ea2733a978987022505891c5ce860db3a72
G O V I N D   U P A D H Y A Y <govind.upadhayay19@gmail.com>
2024-10-17 15:51:08 +0530

    setting up Hero Section

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
+import "../../app/globals.css";
+import Link from "next/link";
 const HeroSection = () => {
   return (
-    <div className=''>
-        <h1 className=''></h1>
+    <div className="hero flex items-center justify-center">
+      <div className="hero-left">
+        <h1 className="text-[5rem] font-extrabold">
+          Creative Designer <br></br>& Developer.
+        </h1>
+        <p className="text-[1rem]">
+          Hi I'm Govind Upadhyay. A passionate Full stack developer based in
+          India❤️
+        </p>
+        <Link href={"/link"}>
+          <li className="relative group cursor-pointer work">
+            <span className="relative z-10">SEE MY WORK</span>
+            <span className="absolute left-0 w-full h-0.5 bg-blue-500 transform scale-x-0 transition-transform duration-300 ease-in-out group-hover:scale-x-100"></span>
+          </li>
+        </Link>
+      </div>
+      <div className="hero-right">
+        <img
+          src="./images/greek.png"
+          className="heroRight-img rounded-full"
+        ></img>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
